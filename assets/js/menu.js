0071cbf2d0ec5f9c189a773820c4fed841bec690
FinalBounce <finalbouncecontact@gmail.com>
2025-06-16 22:31:26 +0200

    new

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,8 +1,6 @@
 
 const burger = document.querySelector('.burger');
 const nav = document.querySelector('.nav-links');
-const navLinks = document.querySelectorAll('.nav-links li');
-
 burger.addEventListener('click', () => {
   nav.classList.toggle('nav-active');
 });
