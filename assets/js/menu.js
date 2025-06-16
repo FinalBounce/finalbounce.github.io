3df1f411a96f0f7b38fca906f8c2c679d157191c
FinalBounce <finalbouncecontact@gmail.com>
2025-06-16 22:49:34 +0200

    fix

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,6 +1,9 @@
 
 const burger = document.querySelector('.burger');
 const nav = document.querySelector('.nav-links');
+const logo = document.querySelector('.logo-container');
+
 burger.addEventListener('click', () => {
   nav.classList.toggle('nav-active');
+  if (logo) logo.classList.toggle('hide-logo');
 });
