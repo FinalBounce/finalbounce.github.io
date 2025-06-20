660d498b5eccb90ad767adcf91dcb53325ff541e
FinalBounce <finalbouncecontact@gmail.com>
2025-06-20 20:44:36 +0200

    Update menu.js

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -3,7 +3,6 @@ const nav    = document.querySelector('.nav-links');
 const body   = document.body;
 
 burger.addEventListener('click', () => {
-  nav.classList.toggle('nav-active');
-  body.classList.toggle('noscroll');
-  document.body.classList.toggle('nav-open');
+  nav.classList.toggle('nav-active');   // fait coulisser le menu
+  body.classList.toggle('nav-open');    // bloque / débloque le scroll général
 });
